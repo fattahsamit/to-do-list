404c3bcfa534d757a07245f73844bbbbe332aed6
fattahsamit <fattahsamit@gmail.com>
2023-05-29 13:29:04 +0600

    todoitem component

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,22 @@
+interface TodoProps {
+  title: string;
+  completed: boolean;
+}
+
+const TodoItem = ({ title, completed }: TodoProps) => {
+  return (
+    <li className={`${completed && "text-success"} my-4 p-2 border rounded`}>
+      <div className="flex flex-col md:flex-row justify-between gap-5">
+        <span className="flex items-center gap-5">
+          <input type="checkbox" checked={completed}></input>
+          {title}
+        </span>
+        <button className="text-white px-2 py-1 rounded-md bg-error">
+          Delete
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default TodoItem;
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,3 +1,5 @@
+import TodoItem from "../TodoItem/TodoItem";
+
 const TodoList = () => {
   const todos = [
     { id: 1, title: "todo1", completed: false },
@@ -9,21 +11,11 @@ const TodoList = () => {
   return (
     <ul className="text-xl w-full md:w-1/3 border p-5 rounded-md mx-auto">
       {todos.map((todo) => (
-        <li
-          className={`${
-            todo.completed && "text-success"
-          } my-4 p-2 border rounded`}
-        >
-          <div className="flex flex-col md:flex-row justify-between gap-5">
-            <span className="flex items-center gap-5">
-              <input type="checkbox" checked={todo.completed}></input>
-              {todo.title}
-            </span>
-            <button className="text-white px-2 py-1 rounded-md bg-error">
-              Delete
-            </button>
-          </div>
-        </li>
+        <TodoItem
+          key={todo.id}
+          title={todo.title}
+          completed={todo.completed}
+        ></TodoItem>
       ))}
     </ul>
   );
