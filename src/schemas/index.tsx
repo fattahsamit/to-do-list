bb797dc8db1ce62b6458312498f8c9ed902da75a
fattahsamit <fattahsamit@gmail.com>
2024-02-13 02:31:37 +0600

    Stronger password validation

--- src/schemas/index.tsx
+++ src/schemas/index.tsx
@@ -14,7 +14,13 @@ export const signUpSchema = Yup.object({
     .max(25)
     .required("Please enter your user name"),
   email: Yup.string().email().required("Please enter your email"),
-  password: Yup.string().min(6).required("Please enter your password"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters")
+    .matches(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).*$/,
+      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    )
+    .required("Please enter your password"),
   confirm_password: Yup.string()
     .required("Please confirm your password")
     .oneOf([Yup.ref("password")], "Password must match"),
